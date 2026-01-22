bb221633c6416e0a82acfd492e38cc0e67b75bda
Shumye Ayalneh <shmuye27@gmail.com>
2026-01-22 09:25:56 +0300

    update user schema

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Name is required'],
         trim: true,
     },
+    avatar: {
+        type: String, // image URL
+        trim: true,
+    },
     email: {
         type: String,
         required: [true, 'Email is required'],
