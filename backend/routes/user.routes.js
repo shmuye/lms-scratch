687e393659bce7f56e89524d32a144d27d38d685
Shumye Ayalneh <shmuye27@gmail.com>
2026-01-29 09:47:48 +0300

    update user route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,10 +6,10 @@ import { createUserByAdmin, getAllUsers, getUserById, getMe, getUserBorrows, upd
 import { ROLES } from '../constants/roles.js';
 import { authMiddleware, permissionMiddleware } from "../middleware/index.js";
 
-router.post('/', authMiddleware, permissionMiddleware([ROLES.ADMIN]), createUserByAdmin);
-router.get('/', authMiddleware, permissionMiddleware([ROLES.ADMIN, ROLES.LIBRARIAN]), getAllUsers);
-router.get('/:id', authMiddleware, permissionMiddleware([ROLES.ADMIN, ROLES.LIBRARIAN]), getUserById);
-router.delete('/:id', authMiddleware, permissionMiddleware([ROLES.ADMIN]), deleteUser);
+router.post('/', authMiddleware, permissionMiddleware("USER_MANAGE"), createUserByAdmin);
+router.get('/', authMiddleware, permissionMiddleware("USER_MANAGE"), getAllUsers);
+router.get('/:id', authMiddleware, permissionMiddleware("USER_MANAGE"), getUserById);
+router.delete('/:id', authMiddleware, permissionMiddleware("USER_MANAGE"), deleteUser);
 router.get('/me/borrows', authMiddleware, getUserBorrows);
 router.get('/me', authMiddleware, getMe);
 router.patch('/me', authMiddleware, updateProfile);
