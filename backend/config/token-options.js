f37ee1523bb6dce103acc3872276e92d036678cd
Shumye Ayalneh <shmuye27@gmail.com>
2026-04-07 22:12:16 +0300

    update refresh token expire duration

--- backend/config/token-options.js
+++ backend/config/token-options.js
@@ -2,12 +2,12 @@ export const ACCESS_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: false,
   sameSite: "lax",
-  maxAge: 1000 * 60 * 5, // 5 minutes,
+  maxAge: 1000 * 60 * 15, // 5 minutes,
 };
 
 export const REFRESH_COOKIE_OPTIONS = {
   httpOnly: true,
   sameSite: "lax",
   secure: false,
-  maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+  maxAge: 24 * 60 * 60 * 1000, // 3 days
 };
