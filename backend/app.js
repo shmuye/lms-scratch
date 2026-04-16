1ce8d7d8adedeeefe57744044daebcce2913a5d0
Shumye Ayalneh <shmuye27@gmail.com>
2026-04-16 16:36:44 +0300

    update cookie configuration

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ ensureUploadFolders();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type"],
--- backend/config/token-options.js
+++ backend/config/token-options.js
@@ -1,7 +1,9 @@
 export const ACCESS_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  // secure: false,
+  secure: true,
+  // sameSite: "lax",
+  sameSite: "none",
   maxAge: 1000 * 60 * 15, // 5 minutes,
 };
 
